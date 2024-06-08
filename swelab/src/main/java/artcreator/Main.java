381c15ae9009bd3085f5fbace52fb1006c76e3da
SamuelMosa <samuel.mosa@web.de>
2024-06-08 12:28:14 +0200

    test commit

--- swelab/src/main/java/artcreator/Main.java
+++ swelab/src/main/java/artcreator/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		CreatorFrame frame = new CreatorFrame();
 	      frame.setVisible(true);
+	      System.out.println("Projekt erfolgreich eingerichtet");
 		
 	}
 
